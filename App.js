ecf82550558192a812365890c92dba2e0790974e
Chris Samuel <ksamuel.chris@icloud.com>
2016-06-13 12:46:55 -0400

    modified the App.js to render without parentheses

--- App.js
+++ App.js
@@ -2,48 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-//state is a collection of values
-	constructor(){
-// 		//initially call super
-		super();
-		this.state = { 
-				txt: 'this is the state txt'
-// 				cat: '0'
-		}
-}
-	update(e){
-		this.setState({txt: e.target.value})
-	}
 	render(){
-		// let txt = this.props.txt
-		// return <h1>{this.state.txt}</h1>
-		return (
-			<div>
-			<input type = "text" 
-				onChange={this.update.bind(this)} />
-				<h1>{this.state.txt}</h1>
-			</div>
-			);
+		return <div> 
+	     <h1> Hello Universe
+		      	</h1>
+			  </div>
+	  }
+}	
 
-		// return (	<h1>Hi</h1> )
-				// <div>
-				   
-						// <b>Bold</b>
-				// </div>
-	}
-}
 
-App.propTypes = {
-	txt : React.PropTypes.string,
-	cat : React.PropTypes.number.isRequired
-}
+export default App
 
-App.defaultProps = {
-	txt: 'this is the default txt'
-}
 
-ReactDOM.render(
-	<App cat={5}  />,
-	document.getElementById('app')
-	);
-// export default App
+
+
