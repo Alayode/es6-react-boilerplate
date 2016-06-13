1a46d2b5f7f8f89b8ebe2cf9b0f54cc82b8565a6
Chris Samuel <ksamuel.chris@icloud.com>
2016-06-12 23:03:48 -0400

    added more changes to the code for using defaultProps nd propTypes

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 class App extends React.Component {
 	render(){
-		return <div>
-				   <h1>Hi</h1> 
-					<b>Bold</b>
-				</div>
+		let txt = this.props.txt
+		return <h1>{txt}</h1>
+		// return <h1>Hello Japan</h1>
+		// return <div>
+		// 		   <h1>Hi</h1> 
+		// 			<b>Bold</b>
+		// 		</div>
 	}
 }
 
-export default App
+App.propTypes = {
+	txt : React.PropTypes.string,
+	cat : React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+	txt: 'this is the default txt '
+}
+ReactDOM.render(
+	<App cat={5}  />,
+	document.getElementById('app')
+	);
+// export default App
