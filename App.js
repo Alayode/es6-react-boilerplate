debb63364b5068de5d594323f10035fbda8ca635
Chris Samuel <ksamuel.chris@icloud.com>
2016-06-13 16:34:21 -0400

    Created a simple props widget for updating the parent component

--- App.js
+++ App.js
@@ -10,12 +10,27 @@ class App extends React.Component{
 	}
 	render(){
 		return ( <div>
-				<input type="text" 
-				   onChange = {this.update}/>
-				<h1>{this.state.txt}</h1>
-			 </div>
+				<Widget txt={this.state.txt} update={this.update} />
+
+				<Widget txt={this.state.txt} update={this.update} />
+
+				<Widget txt={this.state.txt} update={this.update} />
+				
+				<Widget txt={this.state.txt} update={this.update} />
+
+				</div>
 	   );
 	}
+}
+
+// created a const called Widget where props returns the same template
+const Widget = (props) => {
+	return ( <div>
+				<input type="text" 
+				   onChange = {props.update}/>
+				<h1>{props.txt}</h1>
+			 </div>
+	  )
 }
 
 export default App
