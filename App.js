de75d5818344849799e68f329a9f375ece2c94dd
Chris Samuel <ksamuel.chris@icloud.com>
2016-06-15 18:03:22 -0400

    created the Button class that is used in the App class

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-class App extends React.Component{
+class App extends React.Component {
 	render(){
+		 return <Button>React</Button>
 	}
 }
-
-
+class Button extends React.Component {
+	render(){
+		return <button>{this.props.children}</button>
+	}
+}
 export default App
-
