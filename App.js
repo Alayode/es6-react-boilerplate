3b90d99e9be94294f14c35028a74b399dc15b578
Chris Samuel <ksamuel.chris@icloud.com>
2016-07-03 23:54:52 +0200

    component lifecycle updating examples

--- App.js
+++ App.js
@@ -4,17 +4,24 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
 	constructor(){
 		super();
-		this.state = { val: 0 };
+		// this.state = { val: 0 };
+        this.state = {increasing: false}
 		this.update = this.update.bind(this);
 	}
 	update(){
 	this.setState({val: this.state.val + 1})
 }
-componentWillMount(){
-	console.log('mountingNow')
+componentWillRecieveProps(nextProps){
+    this.setState({increasing:nextProps.val > this.props.val })
 }
+shouldComponentUpdate(nextProps,nextState) {
+    return nextProps.val % 5  === 0;
+}
+// componentWillMount(){
+// 	console.log('mountingNow')
+// }
 render(){
-	console.log('rendering!')
+	console.log('this state is increasing')
 	return <button onClick={this.update}>{this.state.val}</button>
 }
 	componentDidMount(){
